10e8b9f03b8752b0bd6d98a4ce65da0dddb149e2
Bella Rose Wilson <bwilson9893@gmail.com>
2025-04-07 17:44:45 +0000

    updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,36 @@
-class ApplicationController < ActionController::Base
-  skip_forgery_protection
+class GameController < ApplicationController
+  def rules
+    render :homepage
+  end
+
+  def rock
+    play("rock")
+    render :zebra
+  end
+
+  def paper
+    play("paper")
+    render :giraffe
+  end
+
+  def scissors
+    play("scissors")
+    render :capybara
+  end
+
+  private
+
+  def play(player_move)
+    moves = ["rock", "paper", "scissors"]
+    @comp_move = moves.sample
+
+    @outcome = case [player_move, @comp_move]
+               when ["rock", "rock"], ["paper", "paper"], ["scissors", "scissors"]
+                 "tied"
+               when ["rock", "scissors"], ["paper", "rock"], ["scissors", "paper"]
+                 "won"
+               else
+                 "lost"
+               end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-
+  root "game#rules"
+  
+  get "rock", to: "game#rock"
+  get "paper", to: "game#paper"
+  get "scissors", to: "game#scissors"
+  get "rules", to: "game#rules"
 end
