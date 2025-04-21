17adb3cffd3802c5f6b4664a1e5d748b17882922
Bella Rose Wilson <bwilson9893@gmail.com>
2025-04-21 18:49:45 +0000

    rules and game controller added

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,13 +1,41 @@
+# app/controllers/game_controller.rb
 class GameController < ApplicationController
+  def rules
+    # This will just render the rules.html.erb
+  end
+
   def rock
+    play("rock")
   end
 
   def paper
+    play("paper")
   end
 
   def scissors
+    play("scissors")
   end
 
-  def rules
+  private
+
+  def play(player_choice)
+    choices = ["rock", "paper", "scissors"]
+    computer_choice = choices.sample
+
+    result = if player_choice == computer_choice
+               "It's a tie!"
+             elsif (player_choice == "rock" && computer_choice == "scissors") ||
+                   (player_choice == "paper" && computer_choice == "rock") ||
+                   (player_choice == "scissors" && computer_choice == "paper")
+               "You win!"
+             else
+               "You lose!"
+             end
+
+    @player_choice = player_choice
+    @computer_choice = computer_choice
+    @result = result
+
+    render :result
   end
 end
